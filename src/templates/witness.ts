cdf383f584f09ab8f8bbd32729f53394ce74968c
afarukcali <afarukcali@gmail.com>
2022-12-29 15:28:22 +0300

    control block review.

--- src/templates/witness.ts
+++ src/templates/witness.ts
@@ -5,14 +5,14 @@ import { UTXO } from "../lib/models/UTXO";
 
 const innerKey = "1dae61a4a8f841952be3a511502d4f56e889ffa0685aa0098773ea2d4309f624";
 
-export const witnessTemplate = (utxo: UTXO[], signatories: Signatories, sigs: string[], script: string, address: string) => {
+export const witnessTemplate = (utxo: UTXO[], signatories: Signatories, sigs: string[], script: string) => {
   const numberOfWitnessElements = WizData.fromNumber(signatories[0].length + 3).hex;
   const degregadingPeriodIndex = utils.compactSizeVarIntData("01");
 
   const scriptCompact = utils.compactSizeVarIntData(script.substring(2));
 
   const tapTweakPrefix =
-    taproot.tapRoot(WizData.fromHex(innerKey), [WizData.fromHex(address.substring(2))], TAPROOT_VERSION.BITCOIN).tweak.hex.substring(0, 2) === "02" ? "c0" : "c1";
+    taproot.tapRoot(WizData.fromHex(innerKey), [WizData.fromHex(script.substring(2))], TAPROOT_VERSION.BITCOIN).tweak.hex.substring(0, 2) === "02" ? "c0" : "c1";
 
   let final = "";
   utxo.forEach((ut, index) => {
